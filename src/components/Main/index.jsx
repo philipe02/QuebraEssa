894c045c4f883d85ba2db26cf89849ff5aa60bbe
Philipe Santos <philipe_02@hotmail.com>
2021-08-23 08:28:11 -0300

    Ajustes nos arquivos base

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>Projeto Hackaton</S.Title>
+    <S.Description>Projeto desenvolvido</S.Description>
+  </S.Wrapper>
+)
+
+export default Main
