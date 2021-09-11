5310ac44d30306ac94a0da9ff8ece01223920204
Moises Requiao <moisesrn@outlook.com>
2021-09-11 11:18:49 -0300

    merge

---
+++ src/components/HalldaFama/index.jsx
@@ -0,0 +1,8 @@
+const HalldaFama = () => {
+  return (
+    <div>
+      <h1>LKJLKJLKJLK</h1>
+    </div>
+  )
+}
+export default HalldaFama
---
+++ src/pages/teste/index.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line prettier/prettier
+
+const teste = () => {
+  return (
+    <div>
+      <h1>LKJLKJLKJLK</h1>
+    </div>
+  )
+}
+export default teste
